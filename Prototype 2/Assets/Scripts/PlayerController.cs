a3c7e3ea75389c959774985f349cca5dad457785
Mariana <nano091306@gmail.com>
2024-03-22 13:49:23 -0700

    on lesson 2.4
    almost done got the scripts down and got all the movements down just a bit left and a little bit more coding

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,10 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
+    public float xRange = 10.0f;
+
+
+    public GameObject projectilePrefab;
     void Start()
     {
         
@@ -14,11 +18,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.x < -10)
+        if(transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+       if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //we will launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
